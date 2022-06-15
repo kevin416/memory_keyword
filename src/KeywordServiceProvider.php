6ceacf434e840fa2b3b05da509dc15f7619ec4e4
Kevin <uncochen@hotmail.com>
2022-06-15 12:02:49 +0100

    new

--- src/KeywordServiceProvider.php
+++ src/KeywordServiceProvider.php
@@ -21,7 +21,7 @@ class KeywordServiceProvider extends ServiceProvider
         ]);
 
         if (config('keyword.use_package_routes')) {
-            Route::group(['prefix' => 'yepos', 'middleware' => ['Factory']], function () {
+            Route::group(['middleware' => ['YeposAdmin']], function () {
                 $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
             });
         }
