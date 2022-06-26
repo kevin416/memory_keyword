1a67ece8afe542ce69aed2be656c54ddb6aee133
Kevin <uncochen@hotmail.com>
2022-06-26 18:48:06 +0100

    update to backend

--- routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Route;
-use Yepos\Keyword\Http\Controllers\KeywordController;
-use Yepos\Keyword\Http\Controllers\LanguageController;
-
-Route::get('/keyword',[KeywordController::class, 'memory_keyword']);
-
--- src/lang/zh/app.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array (
-    'back_to_home' => '回到主页',
-    'submit' => '提交',
-    'exit_system' => '退出系统',
-    'chinese' => '中文',
-    'english' => '英文',
-);
