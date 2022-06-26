fed0cf59bb74b26c8effc9dc77f7f1c64dc264a2
Kevin <uncochen@hotmail.com>
2022-06-26 20:31:29 +0100

    auth

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,14 @@
 <?php
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Request;
 use Yepos\Keyword\Http\Controllers\Api\KeywordController;
 
-//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+// Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
 
-Route::apiResource('keywords',KeywordController::class);
+    Route::apiResource('keywords', KeywordController::class);
+
+});
